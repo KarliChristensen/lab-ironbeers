066b40ef6eb7d8c2fee3fea122c7c6c88c9b6894
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-03-28 21:50:26 +0200

    Completed -styling on random beer missing

--- app.js
+++ app.js
@@ -11,11 +11,27 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-// ...
+hbs.registerPartials(path.join(__dirname + '/views/partials'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.hbs'));
-app.get('/beers', (req, res) => res.sendFile(__dirname + '/views/beers.hbs'));
-app.get('/random', (req, res) => res.sendFile(__dirname + '/views/random.hbs'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random', (req, res) => {
+  punkAPI.getRandom()
+    .then(beerFromApi => {
+      res.render('random', { beer: beerFromApi[0] });
+    })
+    .catch(error => console.log(error));
+});
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
